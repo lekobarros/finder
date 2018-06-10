73cc0d9948f8c4542c562fbf50bc4fc0fc539d24
Alex Vasconcelos <lekobarros@outlook.com>
2018-06-09 23:28:16 -0300

    Release version 2.0.1

--- README.md
+++ README.md
@@ -1,20 +1,20 @@
 # Finder
-Small animated experiment and training CSS and JS, inspired by MusiXMatch APP music identifier.
+Inspired by the animation of identifying a song in the MusiXMatch application on Android, I worked on this project as an experiment on how to turn this app animation into the browser using CSS3 and Javascript.
 [Demo](https://lekobarros.github.io/finder/)
 
 ## Features:
-- CSS Grid and Flexbox.
+- CSS Flexbox.
 - CSS Variables
-- ES6 features
+- ES6 features.
 
 ## Compatibility
 - Only modern browsers.
 
 ## Credits
-- Images by [Last.FM](https://www.last.fm/)
+- Images Albuns by [Last.FM](https://www.last.fm/)
 - SVG Icons by [Flaticon](https://www.flaticon.com)
-- Original animation by [MusicXMath](https://www.musixmatch.com/pt-br)
-- Helpers [Codrops](http://www.codrops.com)
+- Animation by [MusicXMath](https://www.musixmatch.com/pt-br)
+- Thanks for [Codrops](http://www.codrops.com)!
 
 ## License
 This resource can be used freely if integrated or build upon in personal or commercial projects such as websites, web apps and web templates intended for sale. It is not allowed to take the resource "as-is" and sell it, redistribute, re-publish it, or sell "pluginized" versions of it. Free plugins built using this resource should have a visible mention and link to the original work. Always consider the licenses of all included libraries, scripts and images used.
--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 /**
  * main.js
  * https://github.com/lekobarros/finder
+ * version: 2.0.1
  *
  * Licensed under the MIT license.
  * http://www.opensource.org/licenses/mit-license.php
@@ -8,227 +9,226 @@
  * Copyright 2018, Alex Vaconcelos
  * https://github.com/lekobarros/
  */
- 
-;(function(window){
+{
 	'use strict';
- 
-	var options = {
-		"icons" : ["icon-microphone", "icon-matched"],
-		"gradients" : [
+
+	const configs = {
+		imagePatch : "img/",
+		playState : {
+			init : "isWaiting",
+			find : "isFinding",
+			match : "isMatched",
+			lyrics : "isLyrics"
+		},
+		icons : {
+			microphone 	: "icon-microphone",
+			matched 	: "icon-matched"
+		},
+		gradients : [
 			["#08AEEA", "#2AF598"],
 			["#21D4FD", "#B721FF"],
 			["#FBDA61", "#FF5ACD"],
 			["#0093E9", "#80D0C7"],
 			["#FF9A8B", "#FF99AC"],
 			["#08AEEA", "#2AF598"]
-		]
-	}
-
-	var data = {
-		"artist" : [
+		],
+		timeMatch : {
+			find : 3000,
+			changeIcon : 1000,
+			showLyrics : 1500
+		},
+		artist : [
 			{
 				"name" : "Madeon",
-				"picture" : "img/profiles/madeon.jpg",
 				"track" : {
 					"id" : 1,
 					"name" : "Nonsense <span>(Feat. Mark Foster)</span>",
-					"album" : "Adventure Deluxe",
-					"lyric" : "Look, I am here for you\r\nOpen up your eyes and don't look away\r\nAnd I'm dripping down from the fangs\r\nWhen I hear your name, I feel the pain."
+					"album" : "Adventure",
+					"image" : "adventure.jpg",
+					"lyrics" : [
+						"Look, I am here for you",
+						"Open up your eyes and don't look away",
+						"And I'm dripping down from the fangs",
+						"When I hear your name, I feel the pain."
+					]
 				}
-			}
-			,
+			},
 			{
 				"name" : "Porter Robinson",
-				"picture" : "img/profiles/porterrobinson.jpg",
 				"track" : {
 					"id" : 5,
 					"name" : "Fresh Static Snow",
 					"album" : "Worlds",
-					"lyric" : "You fit perfectly to me in the loneliness, melt this curse away\r\nThough I'll never know your name I'll cry for you the same\r\nYou fit perfectly to me in the loneliness\r\nMelt this curse away"
+					"image" : "worlds.jpg",
+					"lyrics" : [
+						"You fit perfectly to me in the loneliness, melt this curse away",
+						"Though I'll never know your name I'll cry for you the same",
+						"You fit perfectly to me in the loneliness",
+						"Melt this curse away"
+					]
 				}
-			}
-			,
+			},
 			{
 				"name" : "Foxes",
-				"picture" : "img/profiles/foxes.jpg",
 				"track" : {
 					"id" : 9,
 					"name" : "Echo",
 					"album" : "Glorious Deluxe",
-					"lyric" : "I can hear your thoughts\r\nPlease don't leave me now\r\nI can't sleep alone\r\nChasing the light until the dawn"
+					"image" : "glorius_deluxe.jpg",
+					"lyrics" : [
+						"I can hear your thoughts",
+						"Please don't leave me now",
+						"I can't sleep alone",
+						"Chasing the light until the dawn"
+					]
 				}
-			}
-			,
+			},
 			{
 				"name" : "Galantis",
-				"picture" : "img/profiles/galantis.jpg",
 				"track" : {
 					"id" : 2,
 					"name" : "Hey Alligator",
 					"album" : "The Aviary",
-					"lyric" : "Hold me tight and pull me under\r\nYou let me drown, you let me dream\r\nWell you should know by now my heart is in your teeth."
+					"image" : "theaviary.jpg",
+					"lyrics" : [
+						"Hold me tight and pull me under",
+						"You let me drown, you let me dream",
+						"Well you should know by now my heart is in your teeth."
+					]
 				}
 			}
 		]
-	};
-	
-	var globalTrack;
-	
-	const /* Consts */
-		sectionListen = 		document.querySelector('.sections'),
-		buttonPlayer = 			document.querySelector('.section-listen__button'),
-		buttonWavesContainer =	document.querySelector('.section-listen__waves'),
-		buttonWaves = 			document.querySelectorAll('.section-listen__waves > span'),
-		buttonPlayerSvgPatch =	document.querySelector('.section-listen__button > svg > use'),
-		buttonResetPlayer =		document.querySelector('.lyric-menu > .lyric-menu__reset'),
-		iconMicrophone =		options.icons[0],
-		iconMatched = 			options.icons[1];
-	
-	const /* Consts .isMatched + .isLyrics  */
-		matchedTrackArtist = 	document.querySelector('.matched__artist'),
-		matchedTrackName =		document.querySelector('.matched__track'),
-		cardLyric = 			document.querySelector('.section-lyrics'),
-		lyricPicture =			document.querySelector('.lyric-header__picture img'),
-		lyricTrackArtist =		document.querySelector('.lyric-header__track .track-artist'),
-		lyricTrackName =		document.querySelector('.lyric-header__track .track-name'),
-		lyricTrackLyric =		document.querySelector('.lyric-body p');
-		
-	/* Functions find track */
-	function getTrackData(dataRand){
-		var /* vars */
-			array = data.artist,
-			rand = dataRand,
-			track = [];
-		
-		for (var i = 0; i < array.length; i++) {
-			if ( i == rand ){
-				var track = {
-					"trackName"			: array[rand].track.name,
-					"trackAlbum" 		: array[rand].track.album,
-					"trackLyric"		: array[rand].track.lyric,
-					"trackArtist"		: array[rand].name,
-					"trackArtistPic"	: array[rand].picture
-				}
+	}
+
+ 	class Track{
+ 		constructor(el){
+			this.DOM = el;
+			this.EL = {};
+			this.track = {};
+			this.init();
+		}
+
+		init(){
+			this.EL.headTrackImage 		=	this.DOM.querySelector('.track__image > img');
+			this.EL.headTrackTitle		= 	this.DOM.querySelector('.track__title > .track__name');
+			this.EL.headTrackArtist		=	this.DOM.querySelector('.track__title > .track__artist');
+			this.EL.listenTrackName		=	this.DOM.querySelector('.listen__track > .track__name');
+			this.EL.listenTrackArtist	=	this.DOM.querySelector('.listen__track > .track__artist');
+			this.EL.Lyrics				= 	this.DOM.querySelector('.section__lyrics');
+			this.EL.LyricsChilds		=	this.DOM.querySelectorAll('.section__lyrics > p');
+		}
+
+		getRandomTrack(){
+			let tracks = configs.artist.length;
+			let RandTrack = Math.floor(Math.random() * tracks);
+			let objTrack = configs.artist[RandTrack];
+
+			this.track = {
+				"id"			: objTrack.track.id,
+				"name"			: objTrack.track.name,
+				"album" 		: objTrack.track.album,
+				"image"			: objTrack.track.image,
+				"lyrics"		: objTrack.track.lyrics,
+				"artist"		: objTrack.name,
 			}
 		}
-		return track;
+
+		setTrackLayout(){
+			this.EL.headTrackImage.setAttribute("src", 	configs.imagePatch + this.track.image);
+			this.EL.headTrackTitle.innerHTML 		=	this.track.name;
+			this.EL.headTrackArtist.innerHTML		=	this.track.artist;
+			this.EL.listenTrackName.innerHTML		=	this.track.name;
+			this.EL.listenTrackArtist.innerHTML		=	this.track.artist;
+
+			// Construct Lyrics
+			this.EL.LyricsChilds		=	this.DOM.querySelectorAll('.section__lyrics > p');
+			if(this.EL.LyricsChilds.length == 0){
+				for(let i = 0; i < this.track.lyrics.length;i++) {
+					let countRowLyrics = this.track.lyrics.length;
+					let childRow = document.createElement("p");
+					childRow.innerHTML = this.track.lyrics[i];
+					this.EL.Lyrics.appendChild(childRow);
+				}
+			}
+			else{
+				for(let i = 0; i < this.EL.LyricsChilds.length;i++) {
+					this.EL.LyricsChilds[i].remove();
+				}
+				for(let i = 0; i < this.track.lyrics.length;i++) {
+					let countRowLyrics = this.track.lyrics.length;
+					let childRow = document.createElement("p");
+					childRow.innerHTML = this.track.lyrics[i];
+					this.EL.Lyrics.appendChild(childRow);
+				}				
+			}
+
+			// Set background rand color
+			let bgColor = configs.gradients;
+			let randBgColor = Math.floor(Math.random() * bgColor.length);
+			let style = `background-color: ${bgColor[randBgColor][0]}; background-image: linear-gradient(45deg, ${bgColor[randBgColor][0]} 0%, ${bgColor[randBgColor][1]} 100%);`;
+			this.EL.Lyrics.setAttribute("style", style);
+		}
+ 	}
+
+	class MusicXMatch{
+		constructor(el){
+			this.DOM = el;
+			this.EL = {};
+			this.track = new Track(this.DOM);
+			this.init();
+		}
+
+		init(){
+			// Dom Elements
+			this.EL.listenButton 	=	this.DOM.querySelector('.listen__button');
+			this.EL.listenIcon		=	this.DOM.querySelector('.button__icon > use');
+			this.EL.listenBorders 	= 	this.DOM.querySelectorAll('.listen__borders span');
+			this.EL.listenWaves 	= 	this.DOM.querySelectorAll('.listen__waves span');
+			this.EL.lyricsReset		=	this.DOM.querySelector('.header__reset');
+			this.initEvents();
+		}
+		initEvents(){
+			this.EL.listenButton.addEventListener('click', () => this.initFind());
+			this.EL.lyricsReset.addEventListener('click', () => this.resetLyrics());
+		}
+		/* Events Button */
+		initFind(){
+			if(this.isAnimating) return;
+			this.isAnimating = true;
+			this.DOM.classList.remove(configs.playState.init);
+			this.DOM.classList.add(configs.playState.find);
+			this.track.getRandomTrack();
+			this.track.setTrackLayout();
+
+			return setTimeout( () => {
+				this.initMatch();
+			}, configs.timeMatch.find);
+		}
+		initMatch(){
+			this.DOM.classList.remove(configs.playState.find);
+			this.DOM.classList.add(configs.playState.match);
+			// change icon
+			setTimeout( () => {
+				this.EL.listenIcon.setAttributeNS('http://www.w3.org/1999/xlink', 'xlink:href', `#${configs.icons.matched}`);
+			}, configs.timeMatch.changeIcon);
+			setTimeout( () => {
+				this.initLyrics();
+				this.isAnimating = !this.isAnimating;
+			}, configs.timeMatch.showLyrics);
+		}
+		initLyrics(){
+			this.DOM.classList.remove(configs.playState.match);
+			this.DOM.classList.add(configs.playState.lyrics);
+			this.EL.listenIcon.setAttributeNS('http://www.w3.org/1999/xlink', 'xlink:href', `#${configs.icons.microphone}`);
+		}
+
+		resetLyrics(){
+			this.DOM.classList.remove(configs.playState.lyrics);
+			this.DOM.classList.add(configs.playState.init);
+		}
+
+
 	}
-	
-	class Track{
-		constructor(track){
-			this.trackName		= track.trackName;
-			this.trackAlbum		= track.trackAlbum;
-			this.trackLyric		= track.trackLyric;
-			this.trackArtist 	= track.trackArtist;
-			this.trackArtistPic	= track.trackArtistPic;			
-		}
-		
-		setMatched(){
-			matchedTrackArtist.innerHTML = this.trackArtist;
-			matchedTrackName.innerHTML = this.trackName;
-		}
-		
-		setLyric(){
-			lyricPicture.setAttribute("src", this.trackArtistPic);
-			lyricTrackArtist.innerHTML = this.trackArtist;
-			lyricTrackName.innerHTML = this.trackName;
-			lyricTrackLyric.innerHTML = this.trackLyric;
-		}
-		
-		setBackgroundCard(){
-			var /* Variables */
-				rand = Math.floor(Math.random() * options.gradients.length),
-				array = options.gradients[rand];
-				
-			var style = `background-color: ${array[0]};background-image: linear-gradient(45deg, ${array[0]} 0%, ${array[1]} 100%);`;
-			cardLyric.setAttribute("style", style);
-		}
-	}
-	
-	/* Functions control sections */
-	function getMatchStats(){
-		var query = sectionListen.className;
-		
-		if ( query.indexOf('isWaiting') ){
-			return 1;
-		}
-		else if	( query.indexOf('isListeling') ){
-			return 2;
-		}
-		else if	( query.indexOf('isMatched') ){
-			return 3;
-		}
-		else if	( query.indexOf('isLyrics') ){
-			return 4;
-		}
-		else{
-			return 0;
-		}
-	}
-	
-	function changeIconNS(icon){
-		return buttonPlayerSvgPatch.setAttributeNS('http://www.w3.org/1999/xlink', 'xlink:href', `#${icon}`);
-	}
-				
-	/* Call class and functions section */
-	function callListeling(){
-		var trackRand = Math.floor(Math.random() * data.artist.length);
-		globalTrack = new Track(getTrackData(trackRand));	
-		sectionListen.classList.remove('isWaiting');
-		sectionListen.classList.add('isListeling');
-		// Call matched music
-		setTimeout(() => {
-			callMatched();
-		}, 3000);
-	}
-	
-	function callMatched(){
-		globalTrack.setMatched();
-		sectionListen.classList.remove('isListeling');
-		sectionListen.classList.add('isMatched');
-		// Call animation waves back-in.
-		setTimeout(() => {
-			changeIconNS(iconMatched);
-		}, 300);
-		// Call the lyrics
-		setTimeout(() => {
-			callLyrics();
-		}, 1500);
-	}
-	
-	function callLyrics(){
-		globalTrack.setLyric();
-		globalTrack.setBackgroundCard()
-		sectionListen.classList.remove('isMatched');
-		sectionListen.classList.add('isLyrics');
-	}
-	
-	function callReset(){
-		changeIconNS(iconMicrophone);
-		sectionListen.classList.remove('isLyrics');
-		sectionListen.classList.add('isWaiting');
-		
-		// Fix Bug: The wave animation, where after "restart" a continuous animation from where it was stopped.
-		var wavesCount = buttonWaves.length;
-		
-		for (var i = 0; i < wavesCount; i++){
-			buttonWaves[i].remove()			
-		}
-		for (var i = 0; i < wavesCount; i++){
-			var span = document.createElement("span")
-			buttonWavesContainer.appendChild(span);
-		}
-	}
-	
-	/* EventListener in Button */
-	buttonPlayer.addEventListener('click', (ev) => {
-		ev.preventDefault();
-		callListeling();	
-	});
-	
-	buttonResetPlayer.addEventListener('click', (ev) => {
-		ev.preventDefault();
-		callReset();	
-	});
-	
-})(window);
+
+	new MusicXMatch(document.querySelector('main'));
+}
